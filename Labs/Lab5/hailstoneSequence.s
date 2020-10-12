9a57b935ff7883d0703f11dda9e303641d8ce324
tyleraharrison <tyleraharrison@gmail.com>
2020-10-12 10:05:05 -0500

    Added return for lab 5

--- Labs/Lab5/hailstoneSequence.s
+++ Labs/Lab5/hailstoneSequence.s
@@ -15,19 +15,24 @@
         .global main
         .type   main, %function
 main:
-        mov  R0, 5         @ R0 is current number
-        mov  R1, 0         @ R1 is count of number of iterations
+        str fp, [sp, #-4]
+        add fp, sp, #0          @ I still don't understand why we do this
+        mov  R0, 5              @ R0 is current number
+        mov  R1, 0              @ R1 is count of number of iterations
 
 again:
-   ADD  R1, R1, #1     @ increment number of iterations
-        ANDS R4, R0, #1     @ test whether R0 is odd
+   ADD  R1, R1, #1              @ increment number of iterations
+        ANDS R4, R0, #1         @ test whether R0 is odd
         BEQ  even
         ADD  R0, R0, R0, LSL #1 @ if odd, set R0 = R0 + (R0 << 1) + 1
-        ADD  R0, R0, #1     @ and repeat (guaranteed R0 > 1)
+        ADD  R0, R0, #1         @ and repeat (guaranteed R0 > 1)
         B    again
 even:
-    MOV  R0, R0, ASR #1 @ if even, set R0 = R0 >> 1
-        SUBS R7, R0, #1     @ and repeat if R0 != 1
+    MOV  R0, R0, ASR #1         @ if even, set R0 = R0 >> 1
+        SUBS R7, R0, #1         @ and repeat if R0 != 1
         BNE  again
-halt:
-    B    halt           @ infinite loop to stop computation
+        MOV R0, R1              @ put that counter inside of r0 to be returned
+        add sp, fp, #0          @ do a lot of stack stuff I don't quite understand
+        ldr fp, [sp], #4
+        bx lr
+
